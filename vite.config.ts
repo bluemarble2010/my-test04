7e278af78b3db8c4657ace83d65edcb79c101f91
bluemarble2010 <bluemarble2010@gmail.com>
2026-07-30 12:17:20 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/my-test04/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
